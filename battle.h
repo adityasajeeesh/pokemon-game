7ef55c46b892dc5398a4e9ea6eff2d6e92fddbce
Aditya Sajeesh <adityasajeesh@student-10-201-12-215.wireless.adelaide.edu.au>
2022-10-13 14:30:00 +1030

    Initial commit

---
+++ battle.h
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+#include "pokemon.h"
+#include "player.h"
+#include "computer.h"
+#ifndef _BATTLE_H
+#define _BATTLE_H
+
+class Battle : public Player, public Computer{
+    private:
+    
+    public:
+};
+
+#endif /* _BATTLE_H */
