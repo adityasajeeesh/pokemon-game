8d664240afc95f6c6b3aa788d8085869215e1b00
Aditya <a1849231@adelaide.edu.au>
2022-10-18 03:04:41 +1030

    Added option to include player's name

--- player.h
+++ player.h
@@ -8,10 +8,16 @@ using namespace std;
 // same code replicated for computer class
 class Player{
     private:
+        string playerName; // stores the player's name
         Pokemon *playerArray = new Pokemon[4]; // array to store pokemon
         int randPlayerNum = 0; // random number generator store, used for random allocation of Pokemon from pokedex
     public:
         Player(){};
+        Player(string name){
+            playerName = name;
+        }
+        void setName(); // sets the player's name
+        string getName(); // returns the player's name
         void addPokemons(); // used to copy Pokemons from Pokedex into player array
         void returnPokemons(); // shows all Pokemon in player array
         bool winLossStatus(); // boolean operator which determines if player loses (all Pokemon have 0 health) or player wins (all computer Pokemon have 0 health)
