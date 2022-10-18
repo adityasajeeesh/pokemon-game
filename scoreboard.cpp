e0baded490b644d92a5665d522da236c4cdb2d1f
Aditya <a1849231@adelaide.edu.au>
2022-10-18 18:21:01 +1030

    Initial commit

---
+++ scoreboard.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+#include<Windows.h>
+#include "pokemon.h"
+#include "player.h"
+#include "computer.h"
+#include "battle.h"
+#include "scoreboard.h"
+using namespace std;
+
+void Scoreboard::lossCount(Player a){
+    for(int i=0; i<4; i++){
+        if (a.playerArray[i].health == 0){
+            playerLoss++;
+        }
+    }
+};
+
+void Scoreboard::lossCount(Computer b){
+    for(int i=0; i<4; i++){
+        if (b.computerArray[i].health == 0){
+            computerLoss++;
+        }
+};};
+
+
+void Scoreboard::resultOutput(Player a, Computer b){
+    if (playerLoss > computerLoss){
+        cout << "The winner is: " << endl;
+        Sleep(700);
+        cout << "Computer!" << endl;
+        Sleep(500);
+        cout << "Better luck next time! Thank you for playing!" << endl;
+    }
+    else if (computerLoss > playerLoss){
+        cout << "The winner is: " << endl;
+        Sleep(700);
+        cout << a.getName() << "!" << endl;
+        Sleep(500);
+        cout << "Congratulations! Thank you for playing!" << endl;
+    }
+    else if (computerLoss == playerLoss){
+        cout << "There is a tie!" << endl;
+        Sleep(300);
+        cout << "No one wins, better luck next time! Thank you for playing!" << endl;
+        Sleep(500);
+    }
+}
---
+++ scoreboard.h
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+#include "pokemon.h"
+#include "player.h"
+#include "computer.h"
+#include "battle.h"
+#ifndef _SCOREBOARD_H
+#define _SCOREBOARD_H
+using namespace std;
+
+class Scoreboard : public Battle{
+    private:
+        int computerLoss = 0; // counts number of Pokemon with health >= 0
+        int playerLoss = 0; // counts number of Pokemon with health >= 0
+    public:
+        Scoreboard(){};
+        void lossCount(Player a);
+        void lossCount(Computer b);
+        void resultOutput(Player a, Computer b);
+};
+#endif
